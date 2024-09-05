1f68a88e4e92e45b8056365ca13cc6a56c4928af
cheory <lgc1150@naver.com>
2024-09-05 18:34:34 +0900

    9월 5일

---
+++ lesson07_oop/src/lesson07_oop/Selection.java
@@ -0,0 +1,21 @@
+package lesson07_oop;
+
+public class Selection {
+	public static void main(String[] args) {
+		// 선택 정렬
+		// 회차 반복문
+		// 최소값 찾기 반복문 내부
+
+		// -최소값이 존재하는 인덱스
+		// -0번째 위치의 값이 위 인덱스에 있던 값과 교환
+		int[] num = { 1, 3, 2, 5 };
+		int min;
+
+		for (int i = 0; i < num.length; i++) {
+			for (int j = 0; j < num.length; j++) {
+				if()
+
+			}
+		}
+	}
+}
