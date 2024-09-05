194fdb74f62d3ca67ca94553d456a7307e25291e
cheory <lgc1150@naver.com>
2024-09-05 16:28:02 +0900

    9월 5일

--- lesson07_oop/src/lesson_oop/MethodMain.java
+++ lesson07_oop/src/lesson_oop/MethodMain.java
@@ -2,11 +2,18 @@ package lesson_oop;
 
 public class MethodMain {
 	public static void main(String[] args) {
-		Param p = new Param();
-		int result = p.add(10, 20);
-		System.out.println(result);
-		System.out.println(p.add(30, 40));
-		System.out.println(new Param().add(40, 50));
+//		int c = Param.max(new int[] { 10, 20, 5, 30, 1, 4, 50 });
+//		System.out.println(c);
+//		System.out.println(Param.max(new int[] {}));
+//		System.out.println(Param.max(null));
+
+//		String str = "가나다라";
+//		String s1 = str.substring(0, 2).concat("AB").substring(1, 3);
+//		System.out.println(s1);
+		System.out.println(Param.min(1, 2, 3, 4, 5, 6));
+		System.out.println(Param.min()); //	길이가 0인 배열
+		System.out.println(Param.min(new int[] {3,4,5})); // 길이가 0인 배열
+		
 	}
 }
 
@@ -17,5 +24,34 @@ class Param {
 
 	static int add2(int a, int b) {
 		return a + b;
+	}
+
+	// 가변 인자
+	static int max(int[] arr) {
+		if (arr == null || arr.length == 0) {
+			return -1;
+		}
+		// 최대값 처리
+		int ret = arr[0];
+		for (int i : arr) {
+			if (ret < i) {
+				ret = i;
+			}
+		}
+		return ret;
+	}
+
+	static int min(int... num) {
+		if (num == null || num.length == 0) {
+			return -1;
+		}
+		// 최대값 처리
+		int ret = num[0];
+		for (int i : num) {
+			if (ret > i) {
+				ret = i;
+			}
+		}
+		return ret;
 	}
 }
---
+++ lesson07_oop/src/lesson_oop/Overloading.java
@@ -0,0 +1,21 @@
+package lesson_oop;
+
+public class Overloading {
+	public static void main(String[] args) {
+		new Operator().multiply(10, 20);
+	}
+}
+
+class Operator {
+	int multiply(int x, int y) {
+		return x * y;
+	}
+
+	double multiply(double x, double y) {
+		return x * y;
+	}
+
+	double multiply(int x, double y) {
+		return x * y;
+	}
+}
