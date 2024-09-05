4723e066a4d49677699f820636ad192dc9501dc2
ghj1150 <104566566+ghj1150@users.noreply.github.com>
2024-09-05 22:25:22 +0900

    Update Selection.java

--- lesson07_oop/src/lesson07_oop/Selection.java
+++ lesson07_oop/src/lesson07_oop/Selection.java
@@ -1,5 +1,7 @@
 package lesson07_oop;
 
+import java.util.Arrays;
+
 public class Selection {
 	public static void main(String[] args) {
 		// 선택 정렬
@@ -8,14 +10,19 @@ public class Selection {
 
 		// -최소값이 존재하는 인덱스
 		// -0번째 위치의 값이 위 인덱스에 있던 값과 교환
-		int[] num = { 1, 3, 2, 5 };
-		int min;
+		int[] num = { 2, 3, 1, 8, 5 };
+		int min = num[0];
 
-		for (int i = 0; i < num.length; i++) {
-			for (int j = 0; j < num.length; j++) {
-				if()
-
+		for (int i = 0; i < num.length - 1; i++) {
+			for (int j = 0; j < num.length - 1 - i; j++) {
+				if (min < num[j]) {
+					min = num[j];
+					int tmp = num[j];
+					num[j] = num[j + 1];
+					num[j + 1] = tmp;
+				}
 			}
 		}
+		System.out.println(Arrays.toString(num));
 	}
 }
