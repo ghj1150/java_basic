6feb91624aee30ae3789976a8e88861859e7259e
cheory <lgc1150@naver.com>
2024-09-06 18:26:49 +0900

    이이이이

--- student/src/student/StudentMain.java
+++ student/src/student/StudentMain.java
@@ -7,11 +7,9 @@ public class StudentMain {
 		// UI
 		// Create Read Update Delete
 		// 배열 길이 늘리기 + car 배열 활용
-//		Scanner scanner = new Scanner(System.in);
-//		StudentService ss = new StudentService();
-		StudentUtils.nextInt("1. 조회 2. 등록 3. 수정 4. 삭제 5. 종료");
+		Scanner scanner = new Scanner(System.in);
+		StudentService ss = new StudentService();
 		while (true) {
-			System.out.println("1. 조회 2. 등록 3. 수정 4. 삭제 5. 종료");
 			int input = StudentUtils.nextInt("1. 조회 2. 등록 3. 수정 4. 삭제 5. 종료");
 			switch (input) {
 			case 1:
