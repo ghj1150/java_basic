b502390e3862feeab8df706ffe222090bc44be7e
ghj1150 <104566566+ghj1150@users.noreply.github.com>
2024-09-25 16:26:38 +0900

    첫커밋

--- lesson01_install/src/lesson01_install/HelloWorld.java
+++ lesson01_install/src/lesson01_install/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 		for(int i = 2; i <= 9; i++) {
 			for(int j = 1; j <= 9; j++) {
 				System.out.printf("%2d * %2d = %2d", i , j, i*j);
-			}
+			} 
 			System.out.println();
 		}
 	}
